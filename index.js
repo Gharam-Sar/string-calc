deb039606d7c9487ebd5411400db52c49d16a3ee
Gharam-Sar <sargharam@gmail.com>
2022-12-08 13:45:19 +0200

    final

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 function Add(nums){
   let num_array=nums.split(",");
   let len=num_array.length;
@@ -10,33 +9,22 @@ function Add(nums){
           if (parseInt(num_array[0])>=0)
             {if(parseInt(num_array[0])>1000)
                num_array[0]="0";
-            return parseInt(num_array[0]);
-            }
-       
+            return parseInt(num_array[0]);            }
           else throw new Error('negatives not allowed,you entered'+ num_array[0]);
-
         }
-       
       case 2:
         if (parseInt(num_array[0])<0 && parseInt(num_array[1])>=0)
         throw new Error('negatives not allowed,you entered'+ num_array[0]);
         else if (parseInt(num_array[1])<0 && parseInt(num_array[0])>=0)
         throw new Error('negatives not allowed,you entered'+ num_array[1]);
         else if (parseInt(num_array[0]<0) && parseInt(num_array[1])<0)
-        {        throw new Error('negatives not allowed,you entered'+ num_array[0]+"," + num_array[1]);
-}
-        else 
-        {
+        {        throw new Error('negatives not allowed,you entered'+ num_array[0]+"," + num_array[1]);}
+        else        {
           if(parseInt(num_array[1])>1000)
           num_array[1]="0";
           if(parseInt(num_array[0])>1000)
           num_array[0]="0";
-return parseInt(num_array[0])+parseInt(num_array[1]);
-
-        }
-
-    }
+return parseInt(num_array[0])+parseInt(num_array[1]);        }    }
     return "args are not valid, try again"
-    
-}
+ }
 module.exports = Add;
--- index.test.js
+++ index.test.js
@@ -7,10 +7,26 @@ test("empty string", () => {
 test("one number", () => {
 
     let str=String(1);
-    expect(Add("1")).toBe(parseInt(str));
+    expect(Add(str)).toBe(parseInt(str));
 });
-test("one negative number", () => {
 
+test("2 numbers", () => {
+    let str1=String(1);
+     let str2=String(3);
+    expect(Add(str1+","+str2)).toBe(parseInt(str1)+parseInt(str2));
+
+});
+
+test("unknown amount of numbers", () => {
+    let str1=String(1);
+    let str2=String(3);
+    let str3=String(1);
+    let str4=String(8);
+    expect(Add(str1+","+str2+","+str3+","+str4)).toBe("args are not valid, try again");
+
+});
+
+test("one negative number", () => {
     let str=String(-1);
     try {
         Add(str);
@@ -18,6 +34,7 @@ test("one negative number", () => {
         expect(e.message).toBe("negatives not allowed,you entered"+str);
     }
 });
+
 test("the first of 2 is a negative number", () => {
 
     let str1=String(-1);
@@ -51,12 +68,6 @@ test(" 2 negative numbers", () => {
         expect(e.message).toBe('negatives not allowed,you entered'+ ss);
     }
 });
-test("2 numbers", () => {
-    let str1=String(1);
-     let str2=String(3);
-    expect(Add(str1+","+str2)).toBe(parseInt(str1)+parseInt(str2));
-
-});
 
 test("1 number  over 1000", () => {
     let str1=String(1002);
@@ -79,16 +90,6 @@ test("seconed of 2 numbers is over 1000", () => {
     let str1=String(2);
     let str2=String(1003);
   expect(Add(str1+","+str2)).toBe(parseInt(str1));
-
-});
-
-
-test("unknown amount of numbers", () => {
-    let str1=String(1);
-    let str2=String(3);
-    let str3=String(1);
-    let str4=String(8);
-    expect(Add(str1+","+str2+","+str3+","+str4)).toBe("args are not valid, try again");
 
 });
 
